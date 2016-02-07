c391e8ae6ef025aa09e7c5f5a78e0f46058d753d
Rachel Black <rachel.black1984@googlemail.com>
2016-02-07 10:15:16 +0000

    express generator and collections route set up

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/collections', function(req, res, next){
+  res.render('collections', {title: 'collections'});
+});
+
+module.exports = router;
