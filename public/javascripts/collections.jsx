aaac63ed2233c6e93746ef025fce6e24e1c940ca
Rachel Black <rachel.black1984@googlemail.com>
2016-02-07 18:21:36 +0000

    background images added to collections

--- public/javascripts/collections.jsx
+++ public/javascripts/collections.jsx
@@ -4,7 +4,7 @@ var Collection = React.createClass({
   // },
   render: function(){
     return(
-      <div className="collection">
+      <div className="collection col-md-4" id={this.props.id}>
         <h2> {this.props.collectionName}</h2>
         <p> {this.props.description} </p>
       </div>
@@ -12,10 +12,12 @@ var Collection = React.createClass({
   }
 });
 
+
+
 React.render(
   <div>
-    <Collection collectionName="Classic" description="Chic and timeless style"/>
-    <Collection collectionName="Causal" description="Casual fashion"/>
-    <Collection collectionName="Fashion Forward" description="Keep your finger on the pulse of the lastes trends"/>
+    <Collection collectionName="Classic" description="Chic and timeless style" id="classic"/>
+    <Collection collectionName="Causal" description="Casual fashion" id="causal"/>
+    <Collection collectionName="Fashion Forward" description="Keep your finger on the pulse of the lastes trends" id="fashion"/>
   </div>,
   document.getElementById('collection-area'));
